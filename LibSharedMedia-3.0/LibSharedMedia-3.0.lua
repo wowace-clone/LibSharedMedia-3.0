360ee8f2dcab9e0140da38df4eaf87b0cea3c41d
funkydude <funkydude@a4917e5d-5c86-4e20-ab6b-f704dede7e63>
2012-04-17 14:21:04 +0000

    use a compat layer for the zhCN font change and bump toc.
    git-svn-id: https://repos.wowace.com/wow/libsharedmedia-3-0/trunk@65 a4917e5d-5c86-4e20-ab6b-f704dede7e63

--- LibSharedMedia-3.0/LibSharedMedia-3.0.lua
+++ LibSharedMedia-3.0/LibSharedMedia-3.0.lua
@@ -98,9 +98,15 @@ if locale == "koKR" then
 elseif locale == "zhCN" then
 	LOCALE_MASK = lib.LOCALE_BIT_zhCN
 --
-	SML_MT_font["伤害数字"]		= [[Fonts\ARKai_C.ttf]]
-	SML_MT_font["默认"]			= [[Fonts\ARKai_T.ttf]]
-	SML_MT_font["聊天"]			= [[Fonts\ARHei.ttf]]
+	if ReportPlayer then -- XXX 4.3.4 compat
+		SML_MT_font["伤害数字"]		= [[Fonts\ARKai_C.ttf]]
+		SML_MT_font["默认"]			= [[Fonts\ARKai_T.ttf]]
+		SML_MT_font["聊天"]			= [[Fonts\ARHei.ttf]]
+	else
+		SML_MT_font["伤害数字"]		= [[Fonts\ZYKai_C.ttf]]
+		SML_MT_font["默认"]			= [[Fonts\ZYKai_T.ttf]]
+		SML_MT_font["聊天"]			= [[Fonts\ZYHei.ttf]]
+	end
 --
 	lib.DefaultMedia["font"] = "默认" -- someone from zhCN please adjust if needed
 --
